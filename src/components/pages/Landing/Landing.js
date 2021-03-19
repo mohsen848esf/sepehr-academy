cd085df0bfcae0a37daeae062220ce9bae80c1c0
mohsen848sf <mohsenesfandiari848@gmail.com>
2021-03-19 16:03:47 +0330

    change header.css

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -42,71 +42,10 @@ const Landing = () => {
     setState(!state)
   }
   return (
-    <Fragment>
+    <section>
       {/* header */}
       <Navar />
-      {/* <section className="header">
-        <div className="indigo">
 
-          <MDBNavbar color="indigo" dark expand="md">
-            <MDBNavbarBrand>
-              <strong className="white-text">Navbar</strong>
-            </MDBNavbarBrand>
-            <MDBNavbarToggler onClick={toggleCollapse} />
-            <MDBCollapse
-              className="container"
-              id="navbarCollapse3"
-              isOpen={state}
-              navbar
-            >
-              <MDBNavbarNav left>
-                <MDBNavItem active>
-                  <MDBNavLink to="#!">Home</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink to="#!">Features</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink to="#!">Pricing</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBDropdown>
-                    <MDBDropdownToggle nav caret>
-                      <span className="mr-2">Dropdown</span>
-                    </MDBDropdownToggle>
-                    <MDBDropdownMenu>
-                      <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">
-                        Another Action
-                      </MDBDropdownItem>
-                      <MDBDropdownItem href="#!">
-                        Something else here
-                      </MDBDropdownItem>
-                      <MDBDropdownItem href="#!">
-                        Something else here
-                      </MDBDropdownItem>
-                    </MDBDropdownMenu>
-                  </MDBDropdown>
-                </MDBNavItem>
-              </MDBNavbarNav>
-              <MDBNavbarNav right>
-                <MDBNavItem>
-                  <MDBFormInline waves>
-                    <div className="md-form my-0">
-                      <input
-                        className="form-control mr-sm-2"
-                        type="text"
-                        placeholder="Search"
-                        aria-label="Search"
-                      />
-                    </div>
-                  </MDBFormInline>
-                </MDBNavItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
-          </MDBNavbar>
-        </div>
-      </section> */}
 
       {/* content */}
       <div className="container">
@@ -456,7 +395,7 @@ const Landing = () => {
           <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
         </div>
       </footer>
-    </Fragment>
+    </section>
   )
 }
 export default Landing
